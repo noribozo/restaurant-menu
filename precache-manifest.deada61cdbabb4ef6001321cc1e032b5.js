0ee869d9296d5138dd095a3929608f7120788a05
noribozo <krasznaynori@gmail.com>
2021-10-25 18:49:02 +0200

    Updates

---
+++ precache-manifest.deada61cdbabb4ef6001321cc1e032b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1e99c15fb3310561934b11a1f07a7c",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "3fe28a2d8070da52a11d",
+    "url": "/restaurant-menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/restaurant-menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe28a2d8070da52a11d",
+    "url": "/restaurant-menu/static/js/main.1b457dbf.chunk.js"
+  },
+  {
+    "revision": "0c17c07f567da9ad53df",
+    "url": "/restaurant-menu/static/js/runtime-main.2e187e2b.js"
+  }
+]);
